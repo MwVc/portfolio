a367a5994e2628472f576a1dcadbf1aac7866543
Victor Mwadime <victormwadime@gmail.com>
2024-10-15 13:14:36 +0300

    Import useTheme and fix typos

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,24 +2,29 @@ import styles from "./SkillsStyles.module.css";
 import checkMarkIconDark from "../../assets/checkmark-dark.svg";
 import checkMarkIconLight from "../../assets/checkmark-light.svg";
 import SkillsList from "../../common/SkillsList";
+import { useTheme } from "../../common/ThemeContent";
 
 function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon =
+    theme === "light" ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillsList src={checkMarkIconDark} skill="HTML" />
-        <SkillsList src={checkMarkIconDark} skill="CSS" />
-        <SkillsList src={checkMarkIconDark} skill="Javascript" />
-        <SkillsList src={checkMarkIconDark} skill="Node.js" />
+        <SkillsList src={checkMarkIcon} skill="HTML" />
+        <SkillsList src={checkMarkIcon} skill="CSS" />
+        <SkillsList src={checkMarkIcon} skill="Javascript" />
+        <SkillsList src={checkMarkIcon} skill="Node.js" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillsList src={checkMarkIconDark} skill="React" />
-        <SkillsList src={checkMarkIconDark} skill="Bootstrap" />
-        <SkillsList src={checkMarkIconDark} skill="Git" />
-        <SkillsList src={checkMarkIconDark} skill="Vite" />
-        <SkillsList src={checkMarkIconDark} skill="Webpack" />
+        <SkillsList src={checkMarkIcon} skill="React" />
+        <SkillsList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillsList src={checkMarkIcon} skill="Git" />
+        <SkillsList src={checkMarkIcon} skill="Vite" />
+        <SkillsList src={checkMarkIcon} skill="Webpack" />
       </div>
     </section>
   );
