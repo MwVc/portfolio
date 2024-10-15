64458686786f1aab07f420e8424781df59ecce0b
Victor Mwadime <victormwadime@gmail.com>
2024-10-15 13:11:53 +0300

    create contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,49 @@
 import styles from "./ContactStyles.module.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section id="contact" className={styles.container}>
+      <h1 className="sectionTitle">Contact</h1>
+      <form action="#">
+        <div className="formGroup">
+          <label htmlFor="name" hidden={true}>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="email" hidden={true}>
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden={true}>
+            Message
+          </label>
+          <textarea
+            name="Message"
+            id="message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <input type="submit" className="hover btn" value="Submit" />
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
