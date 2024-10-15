767a132949d8a80fb95a8fe0c56300de1935d4e3
Victor Mwadime <victormwadime@gmail.com>
2024-10-15 11:53:07 +0300

    Import checkmark icons, styles module and SkillsList component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,28 @@
+import styles from "./SkillsStyles.module.css";
+import checkMarkIconDark from "../../assets/checkmark-dark.svg";
+import checkMarkIconLight from "../../assets/checkmark-light.svg";
+import SkillsList from "../../common/SkillsList";
+
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillsList src={checkMarkIconDark} skill="HTML" />
+        <SkillsList src={checkMarkIconDark} skill="CSS" />
+        <SkillsList src={checkMarkIconDark} skill="Javascript" />
+        <SkillsList src={checkMarkIconDark} skill="Node.js" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillsList src={checkMarkIconDark} skill="React" />
+        <SkillsList src={checkMarkIconDark} skill="Bootstrap" />
+        <SkillsList src={checkMarkIconDark} skill="Git" />
+        <SkillsList src={checkMarkIconDark} skill="Vite" />
+        <SkillsList src={checkMarkIconDark} skill="Webpack" />
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
