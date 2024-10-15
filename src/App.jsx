c1fa0ecb5bdf33267ff1bf30d419eb71c0990a54
Victor Mwadime <victormwadime@gmail.com>
2024-10-15 13:10:56 +0300

    import Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Profile from "./components/Profile/Profile";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
 import "./App.css";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Profile />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }
