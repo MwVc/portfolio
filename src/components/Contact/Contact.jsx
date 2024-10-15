4953d60054a0f88627358f0005ef459e2e20c7a8
Victor Mwadime <victormwadime@gmail.com>
2024-10-15 14:03:12 +0300

    Integrate formspree in Contact component to send emails when message is submitted

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,27 @@
 import styles from "./ContactStyles.module.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact() {
+  const [state, handleSubmit] = useForm("xrbggljj");
+  console.log(state, handleSubmit);
+  if (state.succeeded) {
+    return (
+      <section id="contact" className={styles.container}>
+        <p>
+          <b>Thank you for reaching out!</b> I&apos;ll get back to you as soon
+          as possible,
+        </p>
+      </section>
+    );
+  }
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="#">
+      <form
+        action="https://formspree.io/f/xrbggljj"
+        onSubmit={handleSubmit}
+        method="POST"
+      >
         <div className="formGroup">
           <label htmlFor="name" hidden={true}>
             Name
@@ -16,6 +33,7 @@ function Contact() {
             placeholder="Name"
             required
           />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
         </div>
         <div className="formGroup">
           <label htmlFor="email" hidden={true}>
@@ -28,6 +46,7 @@ function Contact() {
             placeholder="Email"
             required
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className="formGroup">
           <label htmlFor="message" hidden={true}>
@@ -39,8 +58,18 @@ function Contact() {
             placeholder="Message"
             required
           ></textarea>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
         </div>
-        <input type="submit" className="hover btn" value="Submit" />
+        <input
+          type="submit"
+          className="hover btn"
+          value="Submit"
+          disabled={state.submitting}
+        />
       </form>
     </section>
   );
