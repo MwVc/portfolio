dffcb510b5d5fbf362a2fa50fd856782cc34c106
Victor Mwadime <victormwadime@gmail.com>
2024-10-16 14:25:45 +0300

    include target attribute to open links in blank webpage

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export default function ProjectCard({ src, link, title, description }) {
   return (
-    <a href={link}>
+    <a href={link} target="_blank">
       <img src={src} alt={`${title} logo`} className="hover" />
       <h3>{title}</h3>
       <p>{description}</p>
