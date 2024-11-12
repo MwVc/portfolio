877e38b068fec7a2c0fd6a8436dc9dab858061c4
Victor Mwadime <victormwadime@gmail.com>
2024-11-12 15:00:17 +0300

    fix project links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,4 @@
 import styles from "./ProjectStyles.module.css";
-import cvApp from "../../assets/cv-application.png";
 import memoryGame from "../../assets/memory-game.png";
 import weatherApp from "../../assets/weather-app.png";
 import ProjectCard from "../../common/ProjectCard";
@@ -10,20 +9,14 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={cvApp}
-          link={"https://github.com/MwVc/cv-application"}
-          title={"CV-App"}
-          description="Create a resume"
-        />
-        <ProjectCard
           src={memoryGame}
-          link={"https://github.com/MwVc/memory-game"}
+          link={"https://memorize-gif.netlify.app/"}
           title={"Memory Game"}
           description="Challenge your memory!"
         />
         <ProjectCard
           src={weatherApp}
-          link={"https://github.com/MwVc/Weather-App"}
+          link={"https://mwvc.github.io/Weather-App/"}
           title={"Weather App"}
           description="Check local weather"
         />
